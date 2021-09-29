878813afb50129a8e6cae7048f0011fcd702ae44
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-09-29 14:42:04 -0700

    added eventlisteners and getelementbyid's and css

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const nameInput = document.getElementById('name');
-const button = document.getElementById('button');
+const nameButton = document.getElementById('namebutton');
 const firstName = document.getElementById('firstname');
 
-button.addEventListener('click', ()=>{
+nameButton.addEventListener('click', ()=>{
     let newName = nameInput.value;
     firstName.textContent = newName;
+    console.log('clicking');
 });
+
+const colorButton1 = document.getElementById('colorbutton1');
+const colorButton2 = document.getElementById('colorbutton2');
+const colorButton3 = document.getElementById('colorbutton3');
+
+colorButton1.addEventListener('click', ()=>{
+   document.body.style.backgroundColor = "pink"
+});
+
+colorButton2.addEventListener('click', ()=>{
+   document.body.style.backgroundColor = "lightgreen"
+});
+
+colorButton3.addEventListener('click', ()=>{
+    document.body.style.backgroundColor = "lightblue"
+ });
+ 
